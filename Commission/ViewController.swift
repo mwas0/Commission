30d81d3b75eb09ad4d2f99265c58c4c91ba4aa2d
Maya Wasilewski <mwasilewski3454@stu.d214.org>
2021-10-05 13:13:19 -0500

    finished proj no stretch

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    @IBOutlet weak var CommissionPayTextField: UITextField!
+    @IBOutlet weak var BasePayLabel: UILabel!
+    
+
+    let basePay:Double = 500
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        BasePayLabel.text = "\(basePay)"
+        TotalPayLabel.text = ""
+        
     }
-
-
+@IBAction func whenButtonPressed(_ sender: Any)
+    {
+    let dataString = CommissionPayTextField.text!
+    let commissionPay = Double(dataString) ?? 5.0
+    let totalPay = basePay + commissionPay
+    TotalPayLabel.text = "$\(totalPay)"
+    }
+    
 }
 
